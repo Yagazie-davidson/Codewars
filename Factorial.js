6f191567004c8cf7e20313f8ddeb48685f1a1ff7
letam <davidsonletam@gmail.com>
2023-07-25 18:44:25 +0100

    changes

--- Factorial.js
+++ Factorial.js
@@ -1,8 +1,12 @@
 // Using a Loop
 function factorial(num) {
   let result = 1;
-  for (let i = 2; i <= num; i++) {
-    result *= i;
+  if (num === 1) {
+    return 1;
+  } else {
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
   }
-  return result;
 }
